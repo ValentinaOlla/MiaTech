44e41c8483832d69f28a2cdb0b8224a536c1ae3b
ValentinaOlla <valentina_olla@tiscali.it>
2024-12-14 16:05:51 +0100

    used if-else and else-if to create conditions

--- Esercizi_javascript/esercitazioni.js
+++ Esercizi_javascript/esercitazioni.js
@@ -6,4 +6,18 @@ if (number >5) {
     console.log("La variabile è minore di 5");
 }
 
+//If-else and else-if
+let score = parseInt (prompt('Inserisci il tuo voto'));
+
+if (score >= 90) {
+    console.log("Voto ottimo")
+} else if (score >= 70) {
+    console.log("Voto buono")
+} else if (score >= 60) {
+    console.log("Voto sufficiente")
+} else {
+    console.log("Voto insufficiente");
+}
+
+
 
