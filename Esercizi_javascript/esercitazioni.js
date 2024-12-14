38bf54352d7cb034ddfea5ac3de7e4fc1acde8a6
ValentinaOlla <valentina_olla@tiscali.it>
2024-12-14 19:26:55 +0100

    created an object

--- Esercizi_javascript/esercitazioni.js
+++ Esercizi_javascript/esercitazioni.js
@@ -12,20 +12,6 @@ for (let i = 0; i < meta; i++) {
 }
 console.log("L'Array invertito' è: " + numbers);
 
-/*Popolamento Array
-let parole = []
-
-for (let i = 0; i < 5; i++) {
-    let new_word = prompt("Inserisci una parola");
-    parole.push(new_word);
-}
-
-for (let i = 0; i < parole[i].length; i++) {
-    if (parole[i].length % 2 !==0) {
-        console.log(parole[i]);
-    }
-}*/
-
 //Crea un oggetto
 
 let person = {
