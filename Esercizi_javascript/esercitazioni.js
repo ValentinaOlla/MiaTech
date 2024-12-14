f99a7ced7778b1887f03a42594c1de972ddb0589
ValentinaOlla <valentina_olla@tiscali.it>
2024-12-14 16:37:02 +0100

    cleraed the worksheet

--- Esercizi_javascript/esercitazioni.js
+++ Esercizi_javascript/esercitazioni.js
@@ -1,26 +1,3 @@
-//If and else
-let number = 25
-if (number >5) {
-    console.log("La variabile è maggiore di 5");
-} else {
-    console.log("La variabile è minore di 5");
-}
 
-//Switch
-let score = parseInt (prompt("Inserisci il tuo voto"));
-
-switch (true) {
-    case score >=90:
-        console.log("Voto ottimo");
-        break;
-    case score >= 70:
-        console.log("Voto buono");
-        break;
-    case score >= 60:
-         console.log("Voto sufficiente");
-        break;
-    default:
-        console.log("Voto insufficiente");
-    }
 
 
