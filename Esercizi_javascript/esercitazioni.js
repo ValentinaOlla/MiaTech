123bbd3c599ffae667070e92986fcfcf6aadf35c
ValentinaOlla <valentina_olla@tiscali.it>
2024-12-14 17:27:07 +0100

    edit: added a for cicle to get the sum of the array elements

--- Esercizi_javascript/esercitazioni.js
+++ Esercizi_javascript/esercitazioni.js
@@ -1,5 +1,11 @@
-//Array
-let numbers = ["4", "18", "72", "43", "2", "81", "35"]
+//Somma elementi array
+let numbers = [4, 18, 72, 43, 2, 81, 35]
 console.log(numbers);
+
+let somma = 0
+for (let i = 0; i < numbers.length; i++) {
+    somma += numbers[i]
+}
+console.log("La somma è: " + somma);
 
 
