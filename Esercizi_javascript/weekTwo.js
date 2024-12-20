da8752c0a50034deb30146eeb1c846e94820d6f4
ValentinaOlla <valentina_olla@tiscali.it>
2024-12-20 18:24:06 +0100

    edit: added child class

--- Esercizi_javascript/weekTwo.js
+++ Esercizi_javascript/weekTwo.js
@@ -21,3 +21,21 @@ let Volkswagen = new Automobile("Volkswagen", "Maggiolino", "1938");
 Volkswagen.descrizione();
 Volkswagen.aggiungiChilometri(240000);
 Volkswagen.mostraChilometraggio();
+
+class Elettrica extends Automobile {
+    constructor(marca, modello, anno, chilometraggio, autonomia) {
+        super(marca, modello, anno, chilometraggio);
+        this.autonomia = autonomia;
+    }
+    descrizione() {
+        console.log(`Questa auto è una ${this.marca} modello ${this.modello} del ${this.anno}, e ha un'autonomia di ${this.autonomia}.`);
+    }
+    ricarica(km) {
+        this.autonomia += km;
+    }
+}
+
+const autoElettrica = new Elettrica("Volkswagen", "Maggiolino", "1938", 0, 10000)
+autoElettrica.descrizione()
+autoElettrica.ricarica(5000)
+console.log(autoElettrica);
