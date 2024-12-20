497f5d2ab06d26e789505524c17602353144548b
ValentinaOlla <valentina_olla@tiscali.it>
2024-12-20 16:27:44 +0100

    edit: added method and property

--- Esercizi_javascript/weekOne.js
+++ Esercizi_javascript/weekOne.js
@@ -1,19 +1,3 @@
-//Classe e costruttore
-class Automobile {
-    constructor(marca, modello, anno) {
-        this.marca = marca;
-        this.modello = modello;
-        this.anno = anno;
-    }
-    descrizione() {
-        console.log(`Questa auto è una ${this.marca} modello ${this.modello} del ${this.anno}.`);
-    }
-}
-let Volkswagen = new Automobile("Volkswagen", "Maggiolino", "1938");
-Volkswagen.descrizione();
-
-
-
 //forEach & .map
 let numbers = [1, 2, 3, 4, 5]
 numbers.forEach(function(number) {
--- Esercizi_javascript/weekTwo.js
+++ Esercizi_javascript/weekTwo.js
@@ -1,13 +1,23 @@
 //Classe e costruttore
 class Automobile {
-    constructor(marca, modello, anno) {
+    constructor(marca, modello, anno, chilometraggio) {
         this.marca = marca;
         this.modello = modello;
         this.anno = anno;
+        this.chilometraggio = 0
     }
     descrizione() {
         console.log(`Questa auto è una ${this.marca} modello ${this.modello} del ${this.anno}.`);
     }
+    aggiungiChilometri(km) {
+        this.chilometraggio += km;
+    }
+    mostraChilometraggio() {
+        console.log(`Lautomobile a percorso ${this.chilometraggio}km`);
+    }
 }
+
 let Volkswagen = new Automobile("Volkswagen", "Maggiolino", "1938");
 Volkswagen.descrizione();
+Volkswagen.aggiungiChilometri(240000);
+Volkswagen.mostraChilometraggio();
