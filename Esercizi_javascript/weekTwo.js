3bd797c6346693bd84aaf08e8b17a98779adf1d6
ValentinaOlla <valentina_olla@tiscali.it>
2024-12-23 11:41:40 +0100

    added a getter

--- Esercizi_javascript/weekTwo.js
+++ Esercizi_javascript/weekTwo.js
@@ -5,7 +5,11 @@ class Automobile {
         this.marca = marca;
         this.modello = modello;
         this.anno = anno;
-        this.chilometraggio = chilometraggio
+        this.chilometraggio = chilometraggio;
+    } 
+    
+    get chilometraggio() {
+        return this.chilometraggio;
     }
     descrizione() {
         console.log(`Questa auto è una ${this.marca} modello ${this.modello} del ${this.anno}.`);
@@ -88,6 +92,16 @@ autoUsata._controllaChilometri();
 class Camion extends Automobile {
     constructor(marca, modello, anno, chilometraggio) {
         super(marca, modello, anno, chilometraggio);
+        this.caricoMassimo = caricoMassimo;
+        this.caricoAttuale = 0;
+    }
+    descrizione() {
+        console.log(`Questa auto è una ${this.marca} modello ${this.modello} del ${this.anno}, e ha un'autonomia di ${this.autonomia}km. Il suo carico massimo è di ${this.caricoMassimo}kg`);
+    }
+    carica(kg) {
+        if (kg <= this.caricoMassimo - this.caricoAttuale) {
+        this.caricoAttuale += kg;
+        }
     }
 }
 
