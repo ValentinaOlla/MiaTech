b11fb5d04a7d77a9cdddc1432dcdc832a2be64ba
ValentinaOlla <valentina_olla@tiscali.it>
2025-01-09 21:51:52 +0100

    used console.log on different type of data

--- Esercizi_javascript/week3.js
+++ Esercizi_javascript/week3.js
@@ -7,3 +7,16 @@ console.log(`Ciao, io sono ${name} ${surname}.
     Lo scorso dicembre ho compiuto ${age} anni,
     e da pochissimi mesi mi sono trasferita a ${city}.`);
 
+let number = 25
+let string = "This is a string"
+let array = ["case", "libri", "auto", "viaggi"]
+let obj = {
+    marca: "Fiat",
+    modello: "Panda",
+    anno: 2015
+}
+
+console.log(number);
+console.log(string);
+console.log(array);
+console.log(obj);
