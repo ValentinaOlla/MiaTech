5360623a9c087ac7cab3a1ae448c2b520408b6e9
ValentinaOlla <valentina_olla@tiscali.it>
2025-01-15 19:19:26 +0100

    created a function and used a callback

--- Esercizi_javascript/week4.js
+++ Esercizi_javascript/week4.js
@@ -1,4 +1,9 @@
 //Funzione base con callback
 
+function funzioneBase (arg1, arg2, callback) {
+    callback(arg1 + arg2)
+}
 
-
+funzioneBase(21, 55, function(risultato) {
+    console.log(risultato)
+})
