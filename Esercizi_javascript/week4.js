ced0daf857e30222a09be958fa6c4947a134d57c
ValentinaOlla <valentina_olla@tiscali.it>
2025-01-15 19:14:54 +0100

    cleared the worksheet

--- Esercizi_javascript/week4.js
+++ Esercizi_javascript/week4.js
@@ -1,13 +1,4 @@
 //Funzione base con callback
 
-function calcola(func, arg1, arg2) {
-	const risultato = arg1 + arg2;
-    console.log(risultato);
-    }
 
-    function somma(result) {
-        console.log(result);
-    }
-
-calcola(somma, 21, 55);
 
