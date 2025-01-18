352d79cee5bc88bf07c0933ab5a52f077b605743
ValentinaOlla <valentina_olla@tiscali.it>
2025-01-18 15:37:14 +0100

    edit: added a reject condition

--- Esercizi_javascript/week5.js
+++ Esercizi_javascript/week5.js
@@ -1,13 +1,23 @@
 //Creare una promessa semplice
 
 function promessaSemplice(messaggio) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(messaggio);
-      }, 2000);
+    return new Promise((resolve, reject) => {
+        const random = Math.random();
+
+        if(random < 0.5) {
+            reject(new Error("L'operazione non è andata a buon fine"));
+        } else {
+            setTimeout(() => {
+                resolve(messaggio);
+            }, 2000);
+        }
     });
   }
   
-  promessaSemplice("La promessa è stata risolta dopo 2 secondi.").then((messaggio) => {
+  promessaSemplice("La promessa è stata risolta dopo 2 secondi.")
+  .then((messaggio) => {
     console.log(messaggio);
+  })
+  .catch((error) => {
+    console.error("Si è verificato un errore: ", error.message);
   });
