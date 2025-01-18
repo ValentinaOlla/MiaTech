accdd067cec1a8e114463ec11b13e2785f573c60
ValentinaOlla <valentina_olla@tiscali.it>
2025-01-18 13:21:47 +0100

    created two nested callbacks

--- Esercizi_javascript/week4.js
+++ Esercizi_javascript/week4.js
@@ -8,18 +8,28 @@ funzioneBase(21, 55, function(risultato) {
     console.log(risultato)
 })
 
-
-function operazioneSemplice (a, b, callback) {
-    console.log(a + b)
-    callback()
+//Callback annidati
+function primaOperazione(callback) {
+    console.log("inizio prima operazione");
+    setTimeout(() => {
+        const primoRisultato = "Risultato della prima operazione";
+        console.log(primoRisultato);
+    }, 2000);
 }
 
-operazioneSemplice(20, 30, function() {
-    console.log("callback eseguita")
-})
+function secondaOperazione(primoRisultato, callback) {
+    console.log("Inizio seconda operazione");
+    const secondoRisultato = primoRisultato + "modificato";
+    console.log(secondoRisultato);
+}
 
+function callbackFinale(risultatoFinale) {
+    console.log("Risultato finale: ", risultatoFinale);
+}
 
-
+primaOperazione((primoRisultato) => {
+    secondaOperazione(primoRisultato, callbackFinale);
+});
 
 
 
