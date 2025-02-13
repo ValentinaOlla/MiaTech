d2ddb6394ad0a085a1060d70b87251df759df88d
ValentinaOlla <valentina_olla@tiscali.it>
2025-02-13 19:12:48 +0100

    cleared

--- Esercizi_javascript/week4.js
+++ Esercizi_javascript/week4.js
@@ -1,27 +1,5 @@
 //Callback annidati
-function primaOperazione(callback) {
-    console.log("inizio prima operazione");
-    setTimeout(() => {
-        const primoRisultato = "Risultato della prima operazione";
-        console.log(primoRisultato);
-        callback(primoRisultato);
-    }, 2000);
-}
 
-function secondaOperazione(primoRisultato, callback) {
-    console.log("Inizio seconda operazione");
-    const secondoRisultato = primoRisultato + " modificato";
-    console.log(secondoRisultato);
-    callback(secondoRisultato);
-}
-
-function callbackFinale(risultatoFinale) {
-    console.log("Risultato finale: ", risultatoFinale);
-}
-
-primaOperazione((primoRisultato) => {
-    secondaOperazione(primoRisultato, callbackFinale);
-});
 
 /*//Funzione base con callback
 
