b5b38acda65acc224806eba46cfc540a894b9324
ValentinaOlla <valentina_olla@tiscali.it>
2025-02-13 18:21:18 +0100

    created a series of callback

--- Esercizi_javascript/week4.js
+++ Esercizi_javascript/week4.js
@@ -1,4 +1,29 @@
-//Funzione base con callback
+//Callback annidati
+function primaOperazione(callback) {
+    console.log("inizio prima operazione");
+    setTimeout(() => {
+        const primoRisultato = "Risultato della prima operazione";
+        console.log(primoRisultato);
+        callback(primoRisultato);
+    }, 2000);
+}
+
+function secondaOperazione(primoRisultato, callback) {
+    console.log("Inizio seconda operazione");
+    const secondoRisultato = primoRisultato + " modificato";
+    console.log(secondoRisultato);
+    callback(secondoRisultato);
+}
+
+function callbackFinale(risultatoFinale) {
+    console.log("Risultato finale: ", risultatoFinale);
+}
+
+primaOperazione((primoRisultato) => {
+    secondaOperazione(primoRisultato, callbackFinale);
+});
+
+/*//Funzione base con callback
 
 function funzioneBase (arg1, arg2, callback) {
     callback(arg1 + arg2)
@@ -6,8 +31,7 @@ function funzioneBase (arg1, arg2, callback) {
 
 funzioneBase(21, 55, function(risultato) {
     console.log(risultato)
-})
-
+})*/
 
 
 
