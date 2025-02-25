785ddad3385095e8de71fbd61eb0d5925b804de3
ValentinaOlla <valentina_olla@tiscali.it>
2025-02-25 16:19:18 +0100

    cleared the css file

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,15 +1,1 @@
-import { Component } from "react";
 
-class App extends Component {
-
-  title = "HelloWorld";
-
-  render() {
-    return (
-      <h1>{this.title}</h1>
-    )
-  }
-}
-
-
-export default App
