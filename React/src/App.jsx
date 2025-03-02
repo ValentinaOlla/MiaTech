83a5c8db0348165ff02e72a792a03a3061efb271
ValentinaOlla <valentina_olla@tiscali.it>
2025-03-02 13:22:06 +0100

    edit: used the Card component in another main component

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -5,7 +5,12 @@ const App = () => {
   
     return (
       <>
-        
+        <Card>
+          <div>
+          <h2>Valentina Olla</h2>
+          <p>Imparo a usare react!</p>
+          </div>
+        </Card>
       </>
     )
   };
--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-
+import './Index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(<App />)
