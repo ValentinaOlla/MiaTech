fe76e1eee770296ebaae6d3d0650c12b474bb5f9
ValentinaOlla <valentina_olla@tiscali.it>
2025-03-02 09:09:00 +0100

    added a button that increment the counter

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -3,9 +3,16 @@ import { useState, useEffect } from "react";
 const App = () => {
 const [counter, setCounter] = useState(0);
 
+function increment() {
+  setCounter((_counter) => {
+    return _counter + 1;
+  })
+}
+
   return (
     <div>
       <p>Count:{counter}</p>
+      <button onClick={increment}>Click here</button>
       </div>
   )
 };
