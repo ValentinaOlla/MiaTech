2337390c4ad2991c0cefae72dadbe3b8660292f1
ValentinaOlla <valentina_olla@tiscali.it>
2025-03-02 13:20:10 +0100

    cleared the worksheet

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,18 +1,11 @@
 import { useState, useEffect } from "react";
 import Card from "./Card";
-import avatar from "./avatar"
 
 const App = () => {
   
     return (
       <>
-        <Card>
-          <div>
-          <h2>Valentina Olla</h2>
-          <p>Imparo a usare react!</p>
-          <Avatar  />
-          </div>
-        </Card>
+        
       </>
     )
   };
