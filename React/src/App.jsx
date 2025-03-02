33ab50f8831e6821a5655d0a2bb1f8109940ae53
ValentinaOlla <valentina_olla@tiscali.it>
2025-03-02 09:36:52 +0100

    cleared the worksheet

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,36 +1,11 @@
 import { useState, useEffect } from "react";
 
 const App = () => {
-const [counter, setCounter] = useState(0);
 
-function increment() {
-  setCounter((_counter) => {
-    return _counter + 1;
-  })
-}
-
-function decrement() {
-  setCounter((_counter) => {
-    return _counter - 1;
-  })
-}
-
-function reset() {
-  setCounter(0);
-}
-
-useEffect(() => {
-  document.title = `Count: ${counter}`;
-}, [counter]);
 
   return (
     <>
-      <div>Count:{counter}</div>
-      <div>
-      <button onClick={decrement}>-</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={increment}>+</button>
-      </div>
+      
     </>
   )
 };
