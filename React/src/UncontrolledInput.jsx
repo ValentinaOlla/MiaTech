a18ffa7e6b98f029bff8a8e0f2103481ce629917
ValentinaOlla <valentina_olla@tiscali.it>
2025-03-02 11:42:18 +0100

    edit: added a button that displays an alert message with the current input's value when clicked

--- React/src/UncontrolledInput.jsx
+++ React/src/UncontrolledInput.jsx
@@ -3,9 +3,14 @@ import { useRef } from "react";
 const UncontrolledInput = () => {
 const inputRef = useRef(null);
 
+function handleButton() {
+    alert(`Hai scritto: ${inputRef.current.value}`)
+}
+
     return (
         <>
-        <input type="text" ref={inputRef} placeholder="Scrivi qui" />
+        <input type="text" ref={inputRef} placeholder="Scrivi qui" /><br />
+        <button onClick={handleButton}>Click</button>
         </>
     )
 }
