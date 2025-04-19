5cc5d3fd5a1ed4175252884b01db181c5d70cd53
ValentinaOlla <valentina_olla@tiscali.it>
2025-04-19 12:29:29 +0200

    created the addTodo function

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -1,3 +1,15 @@
 import { Todo } from "./types";
 
 const todos: Todo[] = [];
+
+let Id = 1;
+
+function addTodo(title: string): Todo {
+    const newTodo: Todo = {
+        id: Id++,
+        title,
+        completed: false,
+    };
+    todos.push(newTodo);
+    return newTodo;
+}
